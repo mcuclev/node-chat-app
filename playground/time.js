f2de38c5d47cfac5725d2492c9a9aaac4545291c
mcuclev <maxim.cuclev@gmail.com>
2018-02-12 19:38:07 +0200

    adding timestamps

---
+++ playground/time.js
@@ -0,0 +1,19 @@
+//Jan 1st 1970 00:00
+var moment = require('moment');
+
+// var date = new Date();
+// var months = ['Jan', 'Feb'];
+// console.log(date.getMonth());
+
+// var date = moment();
+// date.add(100, 'year').subtract(9, 'months');
+// console.log(date.format('MMM Do, YYYY'));
+
+//10:35 am
+
+var someTimestamp = moment().valueOf();
+console.log(someTimestamp);
+
+var createdAt = 1234;
+var date = moment(createdAt);
+console.log(date.format('h:mm a'));
